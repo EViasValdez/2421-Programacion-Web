0f43a2e302e08ff2af7ab2497159ef28480e3e16
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2021-09-10 20:49:12 -0500

    Ejercicio inconcluso 10/09/21

---
+++ tester.js
@@ -0,0 +1,41 @@
+var num1 =10;
+var num2 =10;
+var reuno;
+var result;
+
+var oper = prompt("Ingresa la operacion realizar");
+
+var prueba=operacion();
+    alert(prueba)
+function operaciones(){
+    if(oper=="sumar"){
+        suma();
+    }
+    if(oper=="restar"){
+        restar();
+    }
+    if(oper="multiplicar"){
+        multiplicacion();
+    }
+    if(oper="dividir"){
+        division();
+    }
+    reuno=resultado;
+    console.log("El resultado es " + resultado)
+}
+
+function suma(){
+    result=num1+num2
+}
+function resta(){
+    result=num1-num2
+}
+function mulitplicacion(){
+    result=num1*num2
+}
+function division(){
+    result=num1/num2
+}
+
+
+
