078eadda669db303fe51008f68f18019c3cae762
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2021-09-17 15:57:12 -0500

    Ejercicios con input, 17/09/21

---
+++ ejercicio4.js
@@ -0,0 +1,22 @@
+document.getElementById("botoncalcular").click(function(){
+    alert();
+});
+
+function calcular(){
+    let n1 = parseInt(document.getElementById("Numero 1").value);
+    let n2 = parseInt(document.getElementById("Numero 2").value);
+    var operacion = document.getElementById("Operacion").value;
+   if(operacion == 1){
+        document.getElementById("Resultado").value = n1 + n2;
+    }
+    else if(operacion == 2){
+        document.getElementById("Resultado").value = n1 - n2;
+    }
+    else if(operacion == 3){
+        document.getElementById("Resultado").value = n1 * n2;
+    }
+    else if(operacion = 4){
+        document.getElementById("Resultado").value = n1 / n2;
+    }
+    
+}
