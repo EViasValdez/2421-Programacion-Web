7abfe5de0929c0e3ffbf6fe03e7c8f1c1524454a
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2021-09-17 17:36:18 -0500

    Ejercicio 15/09/21

--- ejercicio3.js
+++ ejercicio3.js
@@ -23,50 +23,15 @@ var info = {
         },
         "Videoconsolas en posesion":{
                 "Videoconsolas":"PS2",
-                "Videoconsolas":"Wii",
+                "Videoconsolas":"Wii"
         },
         "Generos musicales":{
                 "Generos":"Rock",
                 "Generos":"New Age"
-        }
-    }
-};
-
-console.log(id);
-=======
-var id = {
-    "Informacion personal":{
-    "Nombre":"Eduardo",
-    "Edad":21,
-    "Carrera":"Ingenieria en software",
-    "Universidad":"Universidad Politecnica de Tecamac",
-    "Horario":"Vespertino"
+        },
     },
 };
 
-var info = {
-    "Intereses":{
-        "IDES preferidos":{
-                "IDES":"NetBeans",
-                "IDES":"Atom",
-                "IDES":"WAMP",
-                "IDES":"Visual Studio Code",
-                "IDES":"Visual Studio Community"
-            },
-        "Lenguajes de programacion favoritos":{
-                "Lenguaje":"Java",
-                "Lenguaje":"C#"
-        },
-        "Videoconsolas en posesion":{
-                "Videoconsolas":"PS2",
-                "Videoconsolas":"Wii",
-        },
-        "Generos musicales":{
-                "Generos":"Rock",
-                "Generos":"New Age"
-        }
-    }
-};
-
 console.log(id);
+console.log(info);
 console.log(info);
