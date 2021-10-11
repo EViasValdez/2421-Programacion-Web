5a8a934b956b6ee360220c9f3b36ca4124d2e2bb
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2021-10-11 18:48:04 -0500

    Ejercicio y actividad, 13/09/21

---
+++ ejercicio5.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+    cargarValores();
+});
+
+var total = 0;
+$(".botones").click(function()
+{   
+    var valor = $(this).attr("valor");
+    if(valor == 1){
+        $(this).css("background-color", "white");            
+        total++;
+    }else{
+        $(this).css("background-color", "red");
+        alert("Perdiste, Estos son tus puntos:"+total);
+        location.reload();            
+    }
+
+});
+
+function valorAleatorio(min = 0, max = 1){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function cargarValores(){
+    $(".verificar").each(function(){
+        $(this).attr("valor", valorAleatorio)
+    });
+}
