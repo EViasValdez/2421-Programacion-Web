5d3ac8ccbeb8cee8b369cddb9ceb52450d927d15
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2021-10-27 11:54:04 -0500

    Ejercicio y actividad con PHP, 25/10/21

---
+++ Ejercicio10.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba de XAMPP</title>
+    <script>
+    </script>
+</head>
+<body>
+    <h1>Primer pagina que hago usando php</h1>
+    <?php
+    print "Hola";
+    ?>
+</body>
+</html>
