34ae6771877e468caefec38ba48230c7bfc6a707
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2021-10-29 18:28:03 -0500

    Ejercicio y actividad con PHP, 25/10/21

---
+++ E10Datos.php
@@ -0,0 +1,13 @@
+<?php
+    $nombre = $_GET["nombre"];
+    $apellido = $_GET["apellido"];
+    $sexo = $_GET["sexo"];
+    $edad = $_GET["edad"];
+    $pais = $_GET["pais"];
+
+    if(isset($_GET["pais"])){
+    print "Ingreso exitoso <br>";
+    print "Sea bienvenido <br>";
+    echo "Sus datos son: $nombre, $apellido, $sexo, $edad, $pais <br>";
+    }
+?>
--- Ejercicio10.php
+++ Ejercicio10.php
@@ -4,14 +4,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prueba de XAMPP</title>
-    <script>
-    </script>
+    <link href="styles/style.css" rel="stylesheet" type="text/css">
+    <title>Primer formulario en PHP</title>
 </head>
 <body>
-    <h1>Primer pagina que hago usando php</h1>
-    <?php
-    print "Hola";
-    ?>
+    <style type="text/css">
+        h1{
+            text-align: center;
+            font-family: "Verdana";
+            font-size: 40px;
+        }
+        form{
+            text-align: center;
+            font-size: 20px;
+            font-family: "Arial";
+        }
+    </style>
+    <h1>Formulario</h1>
+    <form action="E10Datos.php" method="get">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" id="nombre">
+        <br>
+        <label for="apellido">Apellido:</label>
+        <input type="text" name="apellido" id="apellido">
+        <br>
+        <label for="edad">Edad:</label>
+        <input type="number" name="edad" id="edad">
+        <br>
+        <label for="sexo">Sexo:</label>
+        <select name="sexo" id="sexo">
+            <option value="hombre">Hombre</option>
+            <option value="mujer">Mujer</option>
+        </select>
+        <br>
+        <label for="pais">Pais:</label>
+        <select name="pais" id="pais">
+            <option value="argentina">Argentina</option>
+            <option value="brasil">Brasil</option>
+            <option value="chile">Chile</option>
+            <option value="colombia">Colombia</option>
+            <option value="peru">Peru</option>
+        </select>
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
