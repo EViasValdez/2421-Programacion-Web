4a187542a3a184db79d318d14dac6e9cd33544ab
Eduardo <edyvival@hotmail.com>
2022-02-16 18:55:56 -0600

    Ejercicio y actividad, 10/09/21

--- tester.js
+++ tester.js
@@ -1,40 +1,40 @@
-var num1 =10;
-var num2 =10;
+var num1 = 10;
+var num2 = 10;
 var result;
 var oper = prompt("Ingresa la operacion a realizar");
-var prueba=operacion();
+var prueba = operacion();
 
 alert(prueba);
 
 function operaciones(){
-    if(oper=="sumar"){
+    if (oper == "sumar"){
         suma();
     }
-    if(oper=="restar"){
+    if (oper == "restar"){
         restar();
     }
-    if(oper="multiplicar"){
+    if (oper = "multiplicar"){
         multiplicacion();
     }
-    if(oper="dividir"){
+    if (oper = "dividir"){
         division();
     }
-    reuno=resultado;
+    reuno = resultado;
     console.log("El resultado es " + resultado)
 }
 
 function suma(){
-    result=num1+num2;
+    result = num1 + num2;
 }
 
 function resta(){
-    result=num1-num2;
+    result = num1 - num2;
 }
 
 function mulitplicacion(){
-    result=num1*num2;
+    result = num1 * num2;
 }
 
 function division(){
-    result=num1/num2;
+    result = num1 / num2;
 }
