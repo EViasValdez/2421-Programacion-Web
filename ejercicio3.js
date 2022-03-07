23961621558eafa7d74c5b8cf0d8e5a1480aadee
Eduardo <edyvival@hotmail.com>
2022-03-07 15:57:49 -0600

    Ejercicio y actividad, 13/09/21

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,35 +1,35 @@
 var id = {
     "Informacion personal":{
-    "Nombre":"Eduardo",
-    "Edad":21,
-    "Carrera":"Ingenieria en software",
-    "Universidad":"Universidad Politecnica de Tecamac",
-    "Horario":"Vespertino"
+    "Nombre": "Eduardo",
+    "Edad": 21,
+    "Carrera": "Ingenieria en software",
+    "Universidad": "Universidad Politecnica de Tecamac",
+    "Horario": "Vespertino"
     },
 };
 
 var info = {
     "Intereses":{
         "IDES preferidos":{
-                "IDE 1":"NetBeans",
-                "IDE 2":"Atom",
-                "IDE 3":"WAMP/MySQL",
-                "IDE 4":"Visual Studio Code",
-                "IDE 5":"Visual Studio Community",
+                "IDE 1": "NetBeans",
+                "IDE 2": "Atom",
+                "IDE 3": "WAMP/MySQL",
+                "IDE 4": "Visual Studio Code",
+                "IDE 5": "Visual Studio Community",
             },
         "Lenguajes de programacion favoritos":{
-                "Lenguaje 1":"Java",
-                "Lenguaje 2":"C#",
-                "Lenguaje 3":"HTML",
-                "Lenguaje 4":"Javascript"
+                "Lenguaje 1": "Java",
+                "Lenguaje 2": "C#",
+                "Lenguaje 3": "HTML",
+                "Lenguaje 4": "Javascript"
             },
         "Videoconsolas en posesion":{
-                "Videoconsola 1":"PS2",
-                "Videoconsola 2":"Wii"
+                "Videoconsola 1": "PS2",
+                "Videoconsola 2": "Wii"
             },
         "Generos musicales":{
-                "Generos":"Rock",
-                "Generos":"New Age"
+                "Generos": "Rock",
+                "Generos": "New Age"
             },
     },
 };
