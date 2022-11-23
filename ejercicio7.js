fa050fc982348590ce56ff5de4e34eeba365da41
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2022-11-23 10:33:04 -0600

    Ejercicios con input, 17/09/21

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,12 +1,12 @@
 $( function() {
     $( "#agarra1" ).draggable();
-  } );
+} );
 $( function() {
     $( "#agarra2" ).draggable();
-  } );
+} );
 $( function() {
     $( "#agarra3" ).draggable();
-  } );
+} );
 $( function() {
     $( "#agarra4" ).draggable();
-  } );
+} );
