9e3281949c3e11d0360d604e7e5885171f1c060e
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-04-14 20:59:18 -0600

    Add files via upload

--- E10Datos.php
+++ E10Datos.php
@@ -5,7 +5,8 @@ $Sexo = $_GET["sexo"];
 $Edad = $_GET["edad"];
 $Pais = $_GET["pais"];
 
-if (isset($_GET["pais"])) {
+if (isset($_GET["pais"]))
+{
     print "Ingreso exitoso <br>";
     print "Sea bienvenido <br>";
     echo "Sus datos son: $Nombre, $Apellido, $Sexo, $Edad, $Pais <br>";
