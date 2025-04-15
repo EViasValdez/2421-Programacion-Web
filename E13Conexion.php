b752fc174a550177ba9e76381f8e96465db3ff9b
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-04-14 20:43:06 -0600

    Add files via upload

--- E13Conexion.php
+++ E13Conexion.php
@@ -7,6 +7,7 @@ class Conexion {
         $Username = "root";
         $Password = "";
         $Database = "2421IS";
+        
         $this->con = mysqli_connect($servername, $Username, $Password, $Database);
         mysqli_query($this->con, "SET NAMES "utf8"");
     }
